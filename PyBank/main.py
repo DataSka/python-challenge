7d1de964372c6a8084dbe4536f0934df12012370
Karen <leswingk@yahoo.com>
2023-07-05 17:54:11 -0400

    Fixed indentations and defined variables

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,68 @@
 import os
-print("Karen")
+import csv
+
+#define variables
+rows = 0
+profit_t = 0
+dif_t = 0
+max_dif = 0
+date_max_dif = ""
+prior_profit = 0
+min_dif = 0
+date_min_dif = ""
+dif = 0
+total_dif = 0
+
+path_in = os.path.join(".","Resources", "budget_data.csv")
+with open(path_in) as infile:
+    reader = csv.reader(infile)
+
+    header = next(reader)
+    print(header)
+    for row in reader:
+        print(row)
+   #file = open('Resources\budget_data.csv', 'r')
+    #next(file)
+
+
+
+# code to skip first header line
+
+        rows = rows + 1
+        if rows > 1:
+            prior_profit = profit
+    #      
+        #row = row.strip()
+        #row = row.split(",")
+        year_date = row[0]
+        profit = row[1]
+        profit = int(profit)
+
+        if rows > 1:
+            dif = profit - prior_profit
+            total_dif = total_dif + dif 
+            
+        if dif > max_dif:
+            max_dif = dif
+            date_max_dif = year_date
+
+        if dif < min_dif:
+            min_dif = dif
+            date_min_dif = year_date
+
+            profit_t = profit_t + profit
+    # print(year_date, profit)
+    infile.close()
+print("Financial Analysis")
+print("------------------------------")
+  
+print("Total Months", rows)
+
+print("Total", profit_t)
+average_dif = total_dif/(rows - 1)
+
+print("Average Change", average_dif)
+print("Greatest Increase in Profits", max_dif)
+print(date_max_dif)
+print("Greatest Decrease in Profits", min_dif)
+print(date_min_dif)
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # python-challenge
+In this challenge I will be analyzing financial records using Python, finding net profit, profit and losses, and the increase and decrease in profit.
+
+Resourses Used:
+Video Recordings UNCC Python lessons
+Video Recording of tutor session
+
+https://adamtheautomator.com/read-csv-in-python/\
+how to read a csv
+
+https://www.w3schools.com/python/python_conditions.asp
+If Else examples
+
+https://www.w3schools.com/python/python_for_loops.asp
+for loops
+
+
+https://www.w3schools.com/python/ref_string_split.asp
+https://python-reference.readthedocs.io/en/latest/docs/str/split.html
+example on how to split a string into a list
+
+https://www.w3schools.com/python/python_conditions.asp
+conditions code example
+
+
+https://www.pythontutorial.net/python-basics/python-write-text-file/
+code for writing a text file in Python
